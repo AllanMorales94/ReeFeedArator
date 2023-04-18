aff1d9d5ace51e919c2f614e9d57c61cd4e9159a
AllanMorales94 <overlydedicated1994@gmail.com>
2023-04-18 16:29:09 -0700

    Refactor item schema

--- re-feed-arator/models/fooditem.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const itemSchema = require('./fooditemSchema');
-
-module.exports = mongoose.model('Item', fooditemSchema);
--- re-feed-arator/src/pages/App/App.jsx
+++ re-feed-arator/src/pages/App/App.jsx
@@ -8,7 +8,6 @@ import AuthPage from '../AuthPage/AuthPage'
 import { Routes, Route } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar'
 import { getUser } from '../../utilities/users-service';
-
 export default function App() {
 
   const [user, setUser] = useState(getUser())
